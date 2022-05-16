fdaac0257dad5bbd9fb968b62cfdf2e20e98edf8
ritaShang <39939433+ritaShang@users.noreply.github.com>
2022-05-16 18:44:21 +0800

    Update train.py

--- train.py
+++ train.py
@@ -183,7 +183,7 @@ if __name__ == '__main__':
         # iteration number = epochs * steps_per_epoch
         # steps_per_epoch为一个epoch里有多少次batch迭代（即一个epoch里有多少个iteration）
         print("############## Step: training begins...")
-        model.fit(distributed_dataset, epochs=40, steps_per_epoch=50,
+        model.fit(distributed_dataset, epochs=100, steps_per_epoch=30,
                                         # validation_steps=valid_im.shape[0]/global_batch_size,
                                         # validation_data=validation_dataset,
                                         callbacks=callbacks
